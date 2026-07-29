f500f256c5ebed43a61f77fa06e38a7c86bb861e
Akshat Singh <vs22022005@gmail.com>
2026-07-30 03:04:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Triplets with Smaller Sum/triplets-with-smaller-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+	public:
+	int countTriplets(int sum, vector<int>& arr) {
+		int n = arr.size();
+		int ans = 0;
+		sort(arr.begin(), arr.end());
+		for (int i = 0 ; i<n - 2; i++) {
+			int left = i + 1;
+			int right = n - 1;
+			while (left<right) {
+				if (arr[i]+arr[left]+arr[right] < sum) {
+					ans += right - left;
+					left++;
+				}
+				else {
+					
+					right--;
+				}
+			}
+		} return ans;
+		
+	}
+};
